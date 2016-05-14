2959392edb900382178c821eac7485db3709dee2
Rami Abraham <r@radesign.ws>
2016-05-13 22:30:58 -0400

    Remove test methods/hooks

--- integrations/class-base.php
+++ integrations/class-base.php
@@ -7,11 +7,7 @@ abstract class AffiliateWP_Store_Credit_Base {
 		$this->init();
 
 		add_action( 'affwp_set_referral_status', array( $this, 'process_payout' ), 10, 3 );
-		add_action( 'affwp_set_referral_status', array( $this, 'edit_payment' ), 10, 3 );
-		add_action( 'affwp_process_update_referral', array( $this, 'edit_payment' ) );
-		add_action( 'affwp_process_update_referral', array( $this, 'process_payout' ) );
-		//add_action( 'affwp_edit_referral_bottom', array( $this, 'get_new_amount' ) );
-
+		add_action( 'affwp_process_update_referral', array( $this, 'process_payout' ), 0 );
 		add_action( 'affwp_add_referral', array( $this, 'process_payout' ) );
 	}
 
@@ -34,25 +30,5 @@ abstract class AffiliateWP_Store_Credit_Base {
 		} elseif( ( 'paid' === $old_status ) && ( 'unpaid' === $new_status ) ) {
 			$this->remove_payment( $referral_id );
 		}
-	}
-	public function get_new_amount() {
-
-		// Get the referral amounts
-		$referral = affwp_get_referral( absint( $_GET['referral_id'] ) );
-		//
-		$old_amount = $referral->amount;
-		$new_amount = $data['amount'];
-
-
-		$current_balance = get_user_meta( $user_id, 'affwp_wc_credit_balance', true );
-
-		$new_balance = $current_balance + $data['amount'];
-
-		// Test retrieval of new amount
-		update_user_meta( $user_id, 'affwp_wc_credit_balance', $new_balance );
-
-		// var dump
-		echo var_dump( $old_amount );
-		echo var_dump( $new_amount );
 	}
 }
