ff2a361a3048f4b0e27ed7913c84695729c65c04
Andrew Munro <andrew@amdrew.com>
2018-02-20 18:30:00 +1300

    Remove get_store_credit() method from dashboard.php #10
    + update calls to affwp_store_credit_balance()

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -10,35 +10,7 @@ class AffiliateWP_Store_Credit_Dashboard {
 	 * @return void
 	 */
 	public function __construct() {
-
 		add_action( 'affwp_affiliate_dashboard_after_campaign_stats', array( $this, 'the_store_credit' ) );
-	}
-
-	/**
-	 * Get the store credit balance available to the affiliate
-	 *
-	 * @access public
-	 * @since 2.1.0
-	 * @return void
-	 */
-	public function get_store_credit() {
-
-		// Get the user id
-		$affiliate_id    = affwp_get_affiliate_id();
-
-		// Get the current store credit balance
-		$current_balance = get_user_meta( $affiliate_id, 'affwp_wc_credit_balance', true );
-
-		// Don't show anything if the affiliate has a zero balance
-		// we may wish to extend this method to provide a default zero-balance
-		// for affiliates that have a store credit balance of zero.
-		if ( $current_balance <= 0 ) {
-			return;
-		}
-
-		$current_balance = affwp_currency_filter( $current_balance );
-
-		return $current_balance;
 	}
 
 	/**
@@ -52,7 +24,7 @@ class AffiliateWP_Store_Credit_Dashboard {
 	public function the_store_credit() {
 
 		// Bail if there is no store credit balance for the affiliate
-		if ( ! $this->get_store_credit() ) {
+		if ( ! affwp_store_credit_balance() ) {
 			return;
 		}
 
@@ -64,7 +36,7 @@ class AffiliateWP_Store_Credit_Dashboard {
 		// Get the store credit available, add to phrase
 		$store_credit = wp_sprintf( ' %1s %2s.',
 			$notice,
-			$this->get_store_credit()
+			affwp_store_credit_balance()
 		);
 
 		?>
