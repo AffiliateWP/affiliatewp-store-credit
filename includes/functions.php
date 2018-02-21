68f6435baf11498ecf7765d825114fb07da321c6
Andrew Munro <andrew@amdrew.com>
2018-02-21 13:46:52 +1300

    Remove return statement to allow $0.00 balances #10
    Also fixes #61

--- includes/functions.php
+++ includes/functions.php
@@ -22,13 +22,6 @@ function affwp_store_credit_balance( $args = array() ) {
 	// Get the current store credit balance.
 	$current_balance = get_user_meta( $user_id, 'affwp_wc_credit_balance', true );
 
-	// Don't show anything if the affiliate has a zero balance
-	// we may wish to extend this method to provide a default zero-balance
-	// for affiliates that have a store credit balance of zero.
-	if ( $current_balance <= 0 ) {
-		return;
-	}
-
 	$current_balance = affwp_currency_filter( affwp_format_amount( $current_balance ) );
 
 	return $current_balance;
