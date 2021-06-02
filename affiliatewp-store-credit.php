2228b0ef421253249e800c6f8f5ff63fecd145a3
Drew Jaynes <drew@affiliatewp.com>
2021-06-01 22:30:54 -0600

    Recreate main plugin file. #131

---
+++ affiliatewp-store-credit.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Store Credit
+ * Plugin URI: https://affiliatewp.com
+ * Description: Pay AffiliateWP referrals as store credit
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Contributors: ryanduff, ramiabraham, mordauk, sumobi, patrickgarman, section214, tubiz, paninapress
+ * Version: 2.3.4
+ * Text Domain: affiliatewp-store-credit
+ */
